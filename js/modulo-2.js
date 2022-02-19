226809621feb9f4ff7004220497a83d2caf1a921
Henrique Neubert <neubert.study@gmail.com>
2022-02-19 15:56:16 -0300

    0.3

--- js/modulo-2.js
+++ js/modulo-2.js
@@ -1,17 +1,106 @@
 $(document).ready(function (){
 
-      $("#botaoHide").click(function(){
-            $(".btn.block-b").hide(2000, function(){ //exemplo de callback incorporado ao .hide
-                  alert("Callback após efeito finalizar")
-            }); 
-            /*   .hide(200) = tempo de transição, também podemos
-                  utilizar os termos (' slow ') e (' fast ')  
-            */
-            console.log('hide');
+
+      //HIDE E SHOW
+      // $("#botaoHide").click(function(){
+      //       $(".btn.block-b").hide(2000, function(){ //exemplo de callback incorporado ao .hide
+      //             alert("Callback após efeito finalizar")
+      //       }); 
+      //       /*   .hide(200) = tempo de transição, também podemos
+      //             utilizar os termos (' slow ') e (' fast ')  
+      //       */
+      //       console.log('hide');
+      // });
+      // $("#botaoShow").click(function(){
+      //       $(".btn.block-b").show(2000);
+      //       console.log('show');
+      // });
+      //HIDE E SHOW
+
+
+      //TOGGLE
+      // $("#botaoHide").click(function(){
+      //       /*
+      //       Forma mais simplificada do efeito hide e show, pois o
+      //       toggle faz estas duas verificações automaticamente 
+      //       */
+      //       $(".btn.block-b").toggle(2000);
+      //       console.log('show e hide');
+      // });
+      //TOGGLE
+      
+
+       //FADEOUT E FADEIN
+      // $("#botaoIn").click(function(){
+      //       /*
+      //       in = surgir usando fade (//?desaparecer / aparecer / é basicamente um outro efeito suave)
+      //       */
+      //       $(".btn.block-b").fadeIn();
+      // });
+      
+      // $("#botaoOut").click(function(){
+      //       /*
+      //       out = sair/esconder usando fade (//?desaparecer / aparecer / é basicamente um outro efeito suave
+      //       */
+      //       $(".btn.block-b").fadeOut();
+      // });
+       //FADEOUT E FADEIN
+
+
+       //FADETOGGLE
+      // $("#botaoToggle").click(function(){
+      //       /*
+      //       variação simplificada onde com uma unica função temos 
+      //       tando o efeito do fade in quanto do fade out
+      //       */        
+      //       $(".btn.block-b").fadeToggle();
+      // });
+       //FADETOGGLE
+
+
+      //SLIDEDOWN E SLIDE UP
+      // $("#flip").click(function(){
+      //       /*
+      //       Forma de fazer blocos surgirem de diferentes direções
+      //       Definição básica: slide Down faz o bloco sutgit de cima para baixo
+      //       slide up faz o bloco surgir de baixo para cima
+      //       */
+      //       $("#painel").slideDown('slow');
+      // });
+      //SLIDEDOWN E SLIDE UP
+      
+      
+      //SLIDETOGGLE
+      // $('#flip').click(function(){
+      //       $('#painel').slideToggle('slow');
+      // });
+      //SLIDETOGGLE
+
+
+      //ANIMAÇÕES DE ELEMENTOS 
+      // $('#efeito').click(function(){
+      //       $(this).animate({
+      //             right: '0'                 
+      //       }, function(){ 
+      //             /*
+      //             fiz um teste usando callback, achei interessante, e acredito que abra algumas
+      //             possibilidades de uso                  
+      //             */
+      //             console.log('estoy aqui');
+      //             $('#efeito').animate({
+      //                   left: '0',
+      //                   right: 'inherit'
+      //             });
+      //       });       
+      //});
+      //ANIMAÇÕES DE ELEMENTOS COM ANIMATE
+
+
+      //ENCADEAMENTO
+      $('#efeito').click(function(){
+            $(this).css('background-color', 'red').slideUp(2000).slideDown(2000).css('color', 'blue');;
       });
-      $("#botaoShow").click(function(){
-            $(".btn.block-b").show(2000);
-            console.log('show');
-      });
+      //ENCADEAMENTO
+
 
 });         
