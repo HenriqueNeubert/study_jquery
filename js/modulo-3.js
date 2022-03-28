d3526f80f49a02940e88f90905c5256a53730155
HenriqueNeubert <neubert.study@gmail.com>
2022-03-28 20:40:00 -0300

    feat: primeiros passos com ajax / pegando arquivos externos / buscando dados de api ViaCEP 1.0

--- js/modulo-3.js
+++ js/modulo-3.js
@@ -1,5 +1,27 @@
 $(document).ready(function (){
 
+//TRABALHANDO COM AJAX
 
+$('#botaoAjax').click(function(){
+      $('.blocoAjax').load('/partials/conteudo-externo-ajax.txt') //pegando um arquivo estranho
+      console.log('ada');
+})
+
+// $('#botaoAjax').mouseover(function(){
+//       $('.blocoAjax').load('/partials/conteudo-externo-ajax.txt') //pegando um arquivo estranho
+//       console.log('ada');
+// })
+
+$('#cep').blur(function(){ //? Evento de Sair
+      var valor = $(this).val(); //? Pega o valor digitado no campo CEP 
+      $.get("https://viacep.com.br/ws/" + valor + "/json/", function(dados, status){ //? Function de callback //$valor = imprime aqui o valor do campo CEP
+            console.log(dados.localidade);
+            $('#cidade').val(dados.localidade)
+            $('#uf').val(dados.uf)
+      });
+})
+
+//!!ASSISTIDO ATÉ 1:57 ( https://www.youtube.com/watch?v=MIogKzdxDYU )
+//TRABALHANDO COM AJAX
 
 });         
