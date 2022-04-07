050deed58c62ffb2e392012b8022bf598a67d010
Henrique Neubert <neubert.study@gmail.com>
2022-04-07 19:28:09 -0300

    feat: metodo de busca jquery (1.2)

--- js/modulo-4.js
+++ js/modulo-4.js
@@ -1,6 +1,22 @@
 $(document).ready(function (){
 
-      
+      $('#busca').on('keyup', function (){ //keyup = tecla for solta //após abre-se um callback
+            var valor = $(this).val().toLowerCase(); //transforma em letra minusculo
+
+            $('#myTable tr').filter(function (){ //filter = filtra pelos itens dentro da tr
+                  $(this).toggle($(this).text().toLowerCase().indexOf(valor) > -1 ) //indexOf = se existir um texto igual
+            })  
+
+      });
+
+      $('#buscaC').keyup(function (){ //keyup = tecla for solta //após abre-se um callback
+            var valor = $(this).val().toLowerCase(); //transforma em letra minusculo
+
+            $('.cards .col').filter(function (){ //filter = filtra pelos itens dentro da tr
+                  $(this).toggle($(this).text().toLowerCase().indexOf(valor) > -1 ) //indexOf = se existir um texto igual
+            })  
+
+      });
 
 //!!ASSISTIDO ATÉ 2:10 ( https://www.youtube.com/watch?v=MIogKzdxDYU )
 
