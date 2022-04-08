44d2114c2edcf3e03fb6c37f3e45778ffeb3f6c9
Henrique Neubert <neubert.study@gmail.com>
2022-04-07 22:20:18 -0300

    feat: iniciado quiz front e validação (1.3)

---
+++ js/modulo-5.js
@@ -0,0 +1,30 @@
+$(document).ready(function (){
+
+      $('.lista-quiz-respostas li').click(function(){
+            var correta = $('.lista-quiz-respostas li.certa').text();
+            if($(this).hasClass('certa')){
+                  //CERTO
+                  $(this).addClass('verde')
+                  $('.proximo').css('opacity', '1')
+                  $('.proximo').css('pointer-events', 'initial')
+            }else{
+                  //ERRADO
+                  $(this).addClass('errada')
+                  $('.game-over').addClass('active')
+                  $('.game-over-resposta').append(correta)
+                  $('.lista-quiz-respostas li').addClass('disabled')
+            }
+      }) 
+
+      // $('.lista-quiz-respostas li').keyup(function (){ //keyup = tecla for solta //após abre-se um callback
+      //       var valor = $(this).val().toLowerCase(); //transforma em letra minusculo
+
+      //       $('.lista-quiz-respostas li').filter(function (){ //filter = filtra pelos itens dentro da tr
+      //             $(this).toggle($(this).text().toLowerCase().indexOf(valor) > -1 ) //indexOf = se existir um texto igual
+      //       })  
+
+      // });
+
+//!!ASSISTIDO ATÉ 2:10 ( https://www.youtube.com/watch?v=MIogKzdxDYU )
+
+});         
